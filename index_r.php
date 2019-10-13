32a8314704a06f5f01ef0e896d6c38db470ed05c
ArchDyre <rohandyre@gmail.com>
2019-10-13 16:03:31 +0200

    Template Initialising

---
+++ index_r.php
@@ -0,0 +1,102 @@
+<?php
+
+    // Includes:
+    // EXAMPLE: ` include "file.php"; `
+
+    session_start();
+
+
+
+
+
+?>
+
+
+
+
+<!doctype html>
+<html lang="en"> <!-- START OF: <html> -->
+  <head> <!-- START OF: <head> -->
+    
+    <!-- CSS Links -->
+    
+        <!-- Link to css normalising sheet. -->
+        <link rel="stylesheet" type="text/css" href="css/normalise.css">
+
+        <!-- Link to css sheet -->
+        <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    
+    <!-- IE 8 and below compatibility with HTML5 -->
+    
+        <!--[if IE]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
+            </script>
+        <![endif]-->  
+    
+    <!-- Declare <meta> variables -->
+    
+        <!-- Declare description of website -->
+        <meta name="description" content="">
+        <!-- Declare author of website. -->
+        <meta name="author" content="Rohan Dyre">
+        <!-- Declare keywords relating to SEO of website content -->
+        <meta name="keywords" content="">
+        <!-- Required meta tags -->
+        <!-- Defines the character encoding used -->
+        <meta charset="utf-8">
+        <!-- Ensure resizing based on device -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+    
+    
+    <!-- Change the title of the website -->
+    <title>Title of my website</title>
+    
+    
+  </head> <!-- END OF: <head> -->
+  
+  
+  <body> <!-- START OF: <body> -->
+   
+    <div class="container"> <!-- START OF: .container -->
+        
+        
+        
+        
+    </div> <!-- END OF: .container -->
+    
+    
+    
+    <!-- jQuery Link -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+	integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+	crossorigin="anonymous"></script>
+   <!-- Popper.js Link -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <!-- Bootstrap JavaScript Link -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+    <!-- Link to own JavaScript sheets -->
+    
+    <script type="text/javascript" src="javascript/javascript.js">
+    </script> <!-- END OF: Link to own JavaScript sheets -->
+    
+    
+    
+    <!-- Optional In-File JavaScript -->
+    <script type="text/javascript">
+      
+      
+      
+      
+    </script> <!-- END OF: Optional In-File JavaScript -->
+    
+    
+  </body> <!-- END OF: <body> -->
+  
+</html> <!-- END OF: <html> -->
